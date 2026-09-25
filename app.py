c94bd9c8c998d6f53c7b5a39e7a45808649c0b46
Azra Baltali <baltaliazra03@gmail.com>
2026-09-25 21:29:43 +0300

    sqlalchemy

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///todo.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
 
 gorevler = [
         {"id":1, "baslik":"Kitap oku", "tamamlandi":False},
